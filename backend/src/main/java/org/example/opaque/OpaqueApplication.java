c67ff75e197d0557075280339df6045cfab8243a
zakaria ahrbil <zakariaahr@gmail.com>
2026-09-01 04:30:40 +0100

    feat: setup backend, frontend and docker-compose

---
+++ backend/src/main/java/org/example/opaque/OpaqueApplication.java
@@ -0,0 +1,14 @@
+package org.example.opaque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpaqueApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OpaqueApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/org/example/opaque/OpaqueApplicationTests.java
@@ -0,0 +1,14 @@
+package org.example.opaque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpaqueApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+}
